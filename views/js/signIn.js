b9665bbac716da6afb17d7494defe1a436c2ca46
kevin <ulrichkev2017@gmail.com>
2018-10-13 01:59:31 +0100

    first

--- views/js/signIn.js
+++ views/js/signIn.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('#connect').on('click', function () {
         window.location = '/';
     });
-    
+
     $('#creer').on('click', function () {
         localStorage.clear();
         var vide = false;
@@ -17,44 +17,39 @@ $(document).ready(function () {
         if (vide) {
             localStorage.setItem('tel', $('#phone').val());
             localStorage.setItem('pseudo', $('#pseudo').val());
-            $.ajax({
-                url: '/signIn',
-                type: 'GET',
-                dataType: 'html',
-                async: false,
-                success: function (data) {
-                    $('body').html(data);
-                    $('.container:eq(0)').append('<br>').append($('<div class="alert alert-warning"></div>').html('<center>Aucun champ ne doit etre vide</center>'));
-                    $('#phone').val(localStorage.getItem('tel'));
-                    $('#pseudo').val(localStorage.getItem('pseudo'));
-                }
-            });
+            $('.container:eq(0)').append('<br>').append($('<div class="alert alert-warning"></div>').html('<center>Aucun champ ne doit etre vide</center>'));
+            $('#phone').val(localStorage.getItem('tel'));
+            $('#pseudo').val(localStorage.getItem('pseudo'));
         } else {
-            $.ajax({
-                url: '/signIn',
-                beforeSend: function (request) {
-                    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                },
-                type: 'POST',
-                dataType: 'html',
-                data: 'TEL=' + $('#phone').val() + '&PSEUDO=' + $('#pseudo').val() + '&PSSWD=' + $('#password').val(),
-                async: false,
-                success: function (data, status, response) {
-                    var error = response.getResponseHeader('sign-in-error');
-                    localStorage.setItem('tel', $('#phone').val());
-                    localStorage.setItem('pseudo', $('#pseudo').val());
-                    $('body').html(data);
-                    if (error) {
-                        switch (error) {
-                            case '1062':
-                                $('.container:eq(0)').append('<br>').append($('<div class="alert alert-danger"></div>').html('<center>Le numero est deja utilise</center>'));
-                                break;
+            var regex = /^(\+237 )(6)[5-9][0-9]{7}/;
+            var regex1 = /^[a-zA-Z0-9_]{4,}$/;
+            if (regex.test($('#phone').val()) && regex1.test($('#password').val())) {
+                $.ajax({
+                    url: '/signIn',
+                    beforeSend: function (request) {
+                        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    },
+                    type: 'POST',
+                    dataType: 'html',
+                    data: 'TEL=' + $('#phone').val() + '&PSEUDO=' + $('#pseudo').val() + '&PSSWD=' + $('#password').val(),
+                    async: false,
+                    success: function (data, status, response) {
+                        var error = response.getResponseHeader('sign-in-error');
+                        localStorage.setItem('tel', $('#phone').val());
+                        localStorage.setItem('pseudo', $('#pseudo').val());
+                        $('body').html(data);
+                        if (error) {
+                            switch (error) {
+                                case '1062':
+                                    $('.container:eq(0)').append('<br>').append($('<div class="alert alert-danger"></div>').html('<center>Le numero est deja utilise</center>'));
+                                    break;
+                            }
                         }
+                        $('#phone').val(localStorage.getItem('tel'));
+                        $('#pseudo').val(localStorage.getItem('pseudo'));
                     }
-                    $('#phone').val(localStorage.getItem('tel'));
-                    $('#pseudo').val(localStorage.getItem('pseudo'));
-                }
-            });
+                });
+            } else $('.container:eq(0)').append('<br>').append($('<div class="alert alert-danger"></div>').html('<center>numero incorrect ou mot de passe trop court</center>'));
         }
         setTimeout(function () {
             $('.alert').remove();
